3b018b47559610f3e804cd79ce63ff5f27fa659e
Camilo Camargo <camilo246800@outlook.com>
2018-07-02 19:42:08 -0500

    update
    documentación

--- model/Producto.php
+++ model/Producto.php
@@ -1,21 +1,45 @@
-<?php
-	
-	Class Producto{
-		private $id;
-		private $id_producto;
-		private $nombre;
-		private $precio;
-		private $correo;
-		private $fecha;
-		private $status;
-
-		public function __GET($k){
-		 return $this->$k; 
-		}
-    	
-    	public function __SET($k, $v){
-    	 return $this->$k = $v; 
-    	}
-	}
-
+<?php
+/*
+Modelo de la notificación, 
+con los atributos y métodos
+magicos para el acceso a datos.
+*/
+	
+
+	Class Producto{
+
+		private $id;
+
+		private $id_producto;
+
+		private $nombre;
+
+		private $precio;
+
+		private $correo;
+
+		private $fecha;
+
+		private $status;
+
+
+
+		public function __GET($k){
+
+		 return $this->$k; 
+
+		}
+
+    	
+
+    	public function __SET($k, $v){
+
+    	 return $this->$k = $v; 
+
+    	}
+
+	}
+
+
+
 ?>
