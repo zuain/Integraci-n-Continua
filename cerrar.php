ec00dfc81f533bf10ca025bfd497b0163ab7f147
germancachope <39679530+germancachope@users.noreply.github.com>
2018-07-02 20:43:58 -0500

    Update cerrar.php

--- cerrar.php
+++ cerrar.php
@@ -1,6 +1,6 @@
 <?php 
 
-session_start();//inicia sesion
+session_start();//inicia sesion 
 session_destroy();//destruye sesion
 header('Location: http://poli.catarco.com/');//cambia la locacion del header
 
