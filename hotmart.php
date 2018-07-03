fc2b7fc8601f5f7759f84fe7c14f8323fffcf738
Camilo Camargo <camilo246800@outlook.com>
2018-07-02 19:32:33 -0500

    Update
    Documentación, mejoras en el insert de datos para pruebas de las notificaciones.

--- hotmart.php
+++ hotmart.php
@@ -1,13 +1,16 @@
 <?php
-
+    //Auto cargador de clases
     require_once 'includes/Autoloap.php';
 
+    //Objeto producto/model
     $producto = new Producto();
+
+    //Controlador
     $setProducto = new ControllerHotmart();
 
-        $producto->__SET('id_producto',$_POST['prod'] ? $_POST['prod'] : '12345');
+        $producto->__SET('id_producto',$_POST['prod'] ? $_POST['prod'] : '00000');
         $producto->__SET('nombre', $_POST['prod_name'] ? $_POST['prod_name'] : 'Test name');
-        $producto->__SET('precio', floatval($_POST['price'] ? $_POST['price'] : '12345'));
+        $producto->__SET('precio', floatval($_POST['price'] ? $_POST['price'] : '00000'));
         $producto->__SET('correo', $_POST['email'] ? $_POST['email'] : 'dev@test.com');
         $producto->__SET('fecha', date('y-m-d'));
         $producto->__SET('status', $_POST['status'] ? $_POST['status'] : 'approved');
