554fab9328b9ea27ad1c0c091d0c555560ec767b
Camilo Camargo <camilo246800@outlook.com>
2018-07-02 19:40:56 -0500

    update
    Documentación desarrollo

--- model/Usuario.php
+++ model/Usuario.php
@@ -1,17 +1,37 @@
-<?php
-	
-	Class Usuario{
-		private $id;
-		private $nombre;
-		private $pass;
-
-		public function __GET($k){
-		 return $this->$k; 
-		}
-    	
-    	public function __SET($k, $v){
-    	 return $this->$k = $v; 
-    	}
-	}
-
+<?php
+/*
+Modelo del usuario, 
+con los atributos y métodos
+magicos para el acceso a datos.
+*/
+	
+
+	Class Usuario{
+
+		private $id;
+
+		private $nombre;
+
+		private $pass;
+
+
+
+		public function __GET($k){
+
+		 return $this->$k; 
+
+		}
+
+    	
+
+    	public function __SET($k, $v){
+
+    	 return $this->$k = $v; 
+
+    	}
+
+	}
+
+
+
 ?>
